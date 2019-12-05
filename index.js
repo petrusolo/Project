d460a2a0ca966143c0203357e82062f050452895
Petru Solovastru <petre20ro2005@yahoo.com>
2019-12-05 22:37:59 +0200

    Modificare

---
+++ index.js
@@ -0,0 +1,26 @@
+function classRoom(capacity){
+    this.capacity =capacity;
+    this.students =[];
+}
+
+function Person(firstName,lastName){
+        this.firstName = firstName;
+        this.lastName  = lastName;
+
+}
+const pers1 = new Person('Bobo','sooo');
+const pers2 = new Person('aa','ss');
+
+//console.log(person);
+  
+
+ classRoom.push(pers1,pers2);
+ console.log(classRoom);
+ 
+
+ 
+ 
+
+
+
+
